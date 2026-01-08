3fcb861c379cd76158259f643984f0ff60ef8e99
escobardecoud <escobardecoud@gmail.com>
2026-01-08 19:13:30 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Optional, if you have specific global styles, otherwise Tailwind handles it via script in index.html
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
